95550bdc9b26eebc7cc1949770fe3ebce6633055
Cedenofofo <fofocedeno8@gmail.com>
2025-10-25 23:46:31 -0600

    Consulta JOIN entre médicos y pacientes

---
+++ doctores/consultas/join_medicos_pacientes.sql
@@ -0,0 +1,6 @@
+
+-- Pacientes con su médico asignado
+SELECT P.Nombre AS Paciente, M.Nombre AS Medico
+FROM Turnos T
+JOIN Pacientes P ON T.IdPaciente = P.IdPaciente
+JOIN Medicos M ON T.IdMedico = M.IdMedico;
